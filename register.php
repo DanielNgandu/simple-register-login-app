43b5331f851ac21a0ee4143bf91f6692f91494e8
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-10-14 21:08:50 +0200

    Minor changes.

--- register.php
+++ register.php
@@ -14,15 +14,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
     if ($stmt = $conn->prepare("INSERT INTO users (username, password) VALUES(?,?)")) {
         // Bind parameters
-        $stmt->bindParam(1,$username, PDO::PARAM_STR); // Attempt to execute the prepared statement
+        $stmt->bindParam(1, $username, PDO::PARAM_STR); // Attempt to execute the prepared statement
         $stmt->bindParam(2, $password, PDO::PARAM_STR); // Attempt to execute the prepared statement
-
         $stmt->execute();
 
         //after successful registration, log user in
         $stmt1 = $conn->prepare("SELECT id,username,password FROM users WHERE username =:username");
         // Bind parameters
         $stmt1->bindParam(":username", $username, PDO::PARAM_STR); // Attempt to execute the prepared statement
+
         if ($stmt1->execute()) {
             //when we find it, then we get that password and then compare with the one inputed
             if ($stmt1->rowCount() == 1) {
@@ -39,6 +39,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     header("location:index.php");
                 } else {
                     echo "Passwords do not match. Please try again!";
+                    header("location:login.php");
+
                 }
 
                 //then show index page
@@ -69,18 +71,18 @@ function sanitize_input($data)
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Registration</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Registration</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div class="container">
-  <h2>User Registration Form</h2>
+    <h2>User Registration Form</h2>
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <div class="form-group">
             <label for="email">Email:</label>
